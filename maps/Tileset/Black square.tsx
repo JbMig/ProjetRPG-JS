bbdefe3c25d7d5bd98e0acab76e828af4d8890bd
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-04-20 09:36:48 +0200

    j'ai juste save mais ça a modifié donc je push

--- maps/Tileset/Black square.tsx
+++ maps/Tileset/Black square.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.4" name="black square" tilewidth="16" tileheight="16" tilecount="1" columns="1">
- <image source="../../gvdfssdfgvsdfvsdqfvsdfvc.png" width="16" height="16"/>
+ <image source="black_square_pnj.png" width="16" height="16"/>
 </tileset>
