46969ea6ac18aedf316e9ad737afcdd620d85396
JbMig <jbmigone@gmail.com>
2022-04-20 03:14:24 +0200

    Quelques maps, on peut les retoucher en cas de difficulté à les intégrer car j'ai cherché les problèmes

---
+++ maps/Tileset/warrior_m.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="warrior_m" tilewidth="16" tileheight="16" tilecount="54" columns="6">
+ <image source="../warrior_m.png" width="96" height="144"/>
+</tileset>
